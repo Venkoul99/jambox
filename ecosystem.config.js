223cf79e6b17c59aecaa5cbc96248d72b6feb68b
Venelin <venkoul99@gmail.com>
2025-02-05 13:03:38 +0200

    fix ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  apps: [
+    {
+      name: "backend",
+      script: "npm",
+      args: "run start:prod",
+      cwd: "/var/www/jambox/apps/api",
+      env: {
+        NODE_ENV: "production",
+      },
+      log_file: "/var/log/pm2/backend-out.log",
+      error_file: "/var/log/pm2/backend-error.log",
+      watch: false,
+      autorestart: true,
+      max_restarts: 5,
+      instances: 1,
+      exec_mode: "cluster",
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+    {
+      name: "frontend",
+      script: "npm",
+      args: "run start:prod",
+      cwd: "/var/www/jambox/apps/web",
+      env: {
+        NODE_ENV: "production",
+      },
+      log_file: "/var/log/pm2/frontend-out.log",
+      error_file: "/var/log/pm2/frontend-error.log",
+      watch: false,
+      autorestart: true,
+      max_restarts: 5,
+      instances: 1,
+      exec_mode: "cluster",
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
